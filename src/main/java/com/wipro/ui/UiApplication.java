9fbb53923887132b5d7a80252cb84549378168bf
NIKHIL GUPTA <nkgupta1805@gmal.com>
2020-06-29 00:39:53 +0530

    Added REST Call

--- src/main/java/com/wipro/ui/UiApplication.java
+++ src/main/java/com/wipro/ui/UiApplication.java
@@ -2,12 +2,19 @@ package com.wipro.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UiApplication {
-
+	
+	@Bean
+	public RestTemplate buildRestTemplate(){
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
 	}
-
+	
+	
 }
--- src/main/java/com/wipro/ui/controller/UIController.java
+++ src/main/java/com/wipro/ui/controller/UIController.java
@@ -1,12 +1,33 @@
 package com.wipro.ui.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UIController {
-	@RequestMapping(value="/home")
+
+	@Autowired
+	RestTemplate rest;
+
+	@RequestMapping(value = "/")
 	public String load() {
 		return "index";
+	}
+
+	@RequestMapping(value = "/getData")
+	public void getData(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		ResponseEntity<String> resp = rest.getForEntity(
+				"https://b7m9hsi226.execute-api.ap-south-1.amazonaws.com/default/callJavaAPI", String.class);
+		PrintWriter print = response.getWriter();
+		print.write(resp.getBody());
 	}
 }
